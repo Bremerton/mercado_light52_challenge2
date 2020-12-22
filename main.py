260811bb1cf271946216aaa909218b79130e7e7b
Bremerton <72746036+Bremerton@users.noreply.github.com>
2020-12-22 06:37:07 -0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Light Level"+input.light_level())
+x=input.light_level()
+while True:
+    if input.light_level() <= 13:
+        light.clear()
+    elif input.light_level() >= 7:
+        light.set_all(light.rgb(0,0,255))
+    else:
+        light.set_all(light.rgb(255,167,0))
